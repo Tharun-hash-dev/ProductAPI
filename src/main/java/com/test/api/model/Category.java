c2f50a10c6a49d4be8fa921807b95aa0a35bd175
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-04 11:14:07 +0530

    Create Category.java

---
+++ src/main/java/com/test/api/model/Category.java
@@ -0,0 +1,12 @@
+package com.test.api.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+  public static class Category {
+        private int id;
+        private String name;
+    }
