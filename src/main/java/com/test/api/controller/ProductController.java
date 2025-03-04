0bcde80f3f1df79777ab2267e4ce6b121f372773
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-04 10:47:46 +0530

    Update ProductController.java

--- src/main/java/com/test/api/controller/ProductController.java
+++ src/main/java/com/test/api/controller/ProductController.java
@@ -1,1 +1,36 @@
+package com.test.api.controller;
+
+import com.test.api.model.*;
+import com.test.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/init")
+    public String initializeData() {
+        return productService.initializeData();
+    }
+
+    @GetMapping("/show_all")
+    public List<Product> showAll() {
+        return productService.showAll();
+    }
+
+    @GetMapping("/categories/{cat_name}")
+    public List<Product> searchByCategory(@PathVariable String cat_name) {
+        return productService.searchByCategory(cat_name);
+    }
+
+    @GetMapping("/tags/{tagname}")
+    public List<Product> searchByTag(@PathVariable String tagname) {
+        return productService.searchByTag(tagname);
+    }
+}
 
