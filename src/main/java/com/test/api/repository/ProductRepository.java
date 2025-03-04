378a8df0ba7f48eb98b7ff5e4f4e34299582bcd9
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-04 10:48:44 +0530

    Update ProductRepository.java

--- src/main/java/com/test/api/repository/ProductRepository.java
+++ src/main/java/com/test/api/repository/ProductRepository.java
@@ -1,1 +1,19 @@
 
+package com.test.api.repository;
+
+import com.test.api.model.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+    @Query("{ 'categories.name' : ?0 }")
+    List<Product> findByCategoriesName(String categoryName);
+
+    @Query("{ 'tags' : ?0 }")
+    List<Product> findByTags(String tagName);
+}
