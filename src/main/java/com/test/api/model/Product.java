30cc1c0eaa2ac8183c6096f4ad32188ad9dffc20
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-04 11:13:19 +0530

    Update Product.java

--- src/main/java/com/test/api/model/Product.java
+++ src/main/java/com/test/api/model/Product.java
@@ -1,1 +1,19 @@
 
+package com.test.api.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+public class Product {
+
+    @Id
+    private String id;
+    private String uuid;
+    private String name;
+    private List<Category> categories;
+    private List<String> tags;
+}
+}
